3435e2d5fd7bdc5d7d7113465dbdab75eea1f5f4
Peter Chay <peterchay88@gmail.com>
2024-05-28 23:07:41 -0600

    adding requirements.txt

--- src/utils/aggregates_util.py
+++ src/utils/aggregates_util.py
@@ -7,5 +7,9 @@ class Aggregates:
                  from_date="2023-01-09", to_date="2023-01-09",):
         # need to figure out logic for optional variables adjusted, sort, and limit
         # If variables are not defined go with example api call
-        self.endpoint = f"/v2/aggs/ticker/{stocks_ticker}/range/{multiplier}/{timespan}/{from_date}/{to_date}"
 
+        self.endpoint = f"/v2/aggs/ticker/{stocks_ticker}/range/{multiplier}/{timespan}/{from_date}/{to_date}"
+        self.request = PolygonRequests()
+
+    def get_aggregates(self):
+        pass
