5611d86b58d7ff9477a8d544c7d11aa2f3326096
Peter Chay <peterchay88@gmail.com>
2024-05-30 00:57:14 -0600

    Moved start_docker.sh and deleted the shell script folder

---
+++ start_docker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Creating the docker image
+echo "-----------------------------------------"
+echo "Spinning up docker container and image"
+echo "-----------------------------------------"
+echo
+echo "Checking to see if polygon_api:latest already exists"
+echo "If it does removing it"
+docker rmi polygon_api:latest
+docker build -t polygon_api:latest .
+
+# If a container named stock_market_framework exists. Stop it and then remove it.
+echo "-----------------------------------------"
+echo "If container stock_market_framework already exists stop it and then delete it"
+echo "-----------------------------------------"
+echo
+docker stop stock_market_framework
+docker rm stock_market_framework
+
+# Creating Docker container
+docker run -d \
+  --name stock_market_framework \
+  -p 8000:8000 \
+  --restart unless-stopped \
+  -v "$(pwd)":/automation \
+  --env-file secrets.env \
+  -it polygon_api:latest /bin/bash
+
+
