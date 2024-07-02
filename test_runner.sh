19c017655dd32b0e3a75c6dc5efbf4f7b187db36
Peter Chay <peterchay88@gmail.com>
2024-07-02 11:44:13 -0600

    Still working on test runner. Issues with log file and report generation

--- test_runner.sh
+++ test_runner.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+custom_datetime=$(date +"%Y_%m_%d_%H:%M:%S")
+
 if  [ $# -eq 0 ]; then
   echo "please provide a test case id to run OR specify all to run all"
 elif [ "$1" = "all" ]; then
   docker exec -it stock_market_framework pytest
 elif [ $# -eq 1 ]; then
-  docker exec -it stock_market_framework pytest -m $1
+  docker exec  -it stock_market_framework pytest -m $1 \
+  --html /framework/tests/reports/results_$1_$custom_datetime.html \
+  --self-contained-html \
+  --color=yes
 fi
 
 
