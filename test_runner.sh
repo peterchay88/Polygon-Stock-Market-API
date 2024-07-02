2a81ebeb03c86413fd58ed4f0b4d4228e246be2f
Peter Chay <peterchay88@gmail.com>
2024-07-02 11:26:01 -0600

    added to test runner script

--- test_runner.sh
+++ test_runner.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-docker exec -it stock_market_framework pytest -m $1
+if  [ $# -eq 0 ]; then
+  echo "please provide a test case id to run OR specify all to run all"
+elif [ "$1" = "all" ]; then
+  docker exec -it stock_market_framework pytest
+elif [ $# -eq 1 ]; then
+  docker exec -it stock_market_framework pytest -m $1
+fi
 
 
 
