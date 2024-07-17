43d77d25d58e8e3776a3ee4e76ffef488f6c3618
Peter Chay <peterchay88@gmail.com>
2024-07-16 20:57:31 -0600

    added logging

--- framework/tests/ticker_details/test_ticker_details_negative.py
+++ framework/tests/ticker_details/test_ticker_details_negative.py
@@ -3,6 +3,7 @@ from framework.src.utils.logging_util import Logger
 from framework.src.utils.ticker_details_util import TickerDetailsUtility
 
 ticker = TickerDetailsUtility()
+logger = Logger()
 
 
 class TestTickerDetailsNegative:
@@ -14,8 +15,11 @@ class TestTickerDetailsNegative:
         endpoint with the wrong API key
         :return:
         """
+        logger.info("Running test case 5")
         response = ticker.get_ticker_details(api_key="apiKey=123", expected_status_code=401)
         expected_msg = "Unknown API Key"
+        logger.debug(f"Checking to see if received message '{expected_msg}' "
+                     f"matches the expected message '{expected_msg}'")
         assert response.json()['error'] == expected_msg, \
             f"ERROR! Unexpected value, Expected: {expected_msg}. Actual: {response.json()['error']}"
 
@@ -26,8 +30,11 @@ class TestTickerDetailsNegative:
         endpoint with no API key
         :return:
         """
+        logger.info("Running test case 6")
         response = ticker.get_ticker_details(api_key="", expected_status_code=401)
         expected_msg = "API Key was not provided"
+        logger.debug(f"Checking to see if received message '{expected_msg}' "
+                     f"matches the expected message '{expected_msg}'")
         assert response.json()['error'] == expected_msg, \
             f"ERROR! Unexpected value, Expected: {expected_msg}. Actual: {response.json()['error']}"
 
