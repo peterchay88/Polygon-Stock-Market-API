c3777eed2e65e2eb1e8725b579be58cf108beb8c
Peter Chay <peterchay88@gmail.com>
2024-07-18 14:37:31 -0600

    tcid09

--- framework/src/utils/conditions_util.py
+++ framework/src/utils/conditions_util.py
@@ -5,5 +5,35 @@ from framework.src.utils.logging_util import Logger
 class Conditions:
 
     def __init__(self):
-        pass
+        self.endpoint = "/v3/reference/conditions?"
+        self.api_request = PolygonRequests()
+
+    # def get_conditions(self, api_key, asset_class="", data_type="", condition_id="",
+    #                    sip="", order="", limit="", sort=""):
+    #     """
+    #     Get response for the conditions endpoint.
+    #     :param api_key:
+    #     :return:
+    #     """
+    #     response = self.api_request.get(endpoint=f"{self.endpoint}{asset_class}")
+
+    def get_conditions(self, api_key, **kwargs):
+        """
+        Get response for the conditions endpoint.
+        :param api_key:
+        :return:
+        """
+        arguments = []
+        parameters = ["asset_class", "data_type", "id", "sip", "order", "limit", "sort"]
+        index = 0
+        for parameter in parameters:
+            if kwargs.get(parameter[index]):
+                arguments.append(f"{parameters[index]}={kwargs[parameter[index]]}")
+                index =+ 1
+        endpoint = f"{self.endpoint}'&'.join(arguments)"
+        response = self.api_request.get(endpoint=f"{endpoint}&{api_key}")
+        return response
+
+
+
 
--- framework/tests/conditions/test_get_conditions_positive.py
+++ framework/tests/conditions/test_get_conditions_positive.py
@@ -1,14 +1,20 @@
 import pytest
 from framework.src.utils.logging_util import Logger
+from framework.src.utils.conditions_util import Conditions
+
+conditions = Conditions()
+pytestmark = [pytest.mark.conditions_positive]
 
 
 class TestConditionsPositive:
 
     @pytest.mark.tcid09
-    def test_get_conditions(self):
+    def test_get_conditions(self, get_api_key):
         """
         This test case confirms we are able to successfully make a GET request to the conditions endpoint
         :return:
         """
-        pass
-    
+        response = conditions.get_conditions(api_key=get_api_key)
+        import pdb; pdb.set_trace()
+
+
