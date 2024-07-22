94a1a07a9c7cb4ccbcdf887cb36167f7558752af
Peter Chay <peterchay88@gmail.com>
2024-07-22 10:24:03 -0600

    tcid13

--- framework/tests/conditions/test_get_conditions_negative.py
+++ framework/tests/conditions/test_get_conditions_negative.py
@@ -16,6 +16,7 @@ class TestConditionsNegative:
         to the conditions endpoint with no api key
         :return:
         """
+        logger.info("Now running Test case 11")
         response = conditions.get_conditions(api_key="", expected_status_code=401)
         expected_msg = "API Key was not provided"
         assert response.json()["error"] == expected_msg, \
@@ -28,17 +29,24 @@ class TestConditionsNegative:
         to the conditions endpoint with no api key
         :return:
         """
+        logger.info("Now running Test case 12")
         response = conditions.get_conditions(api_key="apiKey=123", expected_status_code=401)
         expected_msg = "Unknown API Key"
         assert response.json()["error"] == expected_msg, \
             f"Error! Unexpected value. Expected: {expected_msg}. Actual {response.json()['error']}"
 
     @pytest.mark.tcid13
-    def test_get_conditions_invalid_option_asset_class(self):
+    def test_get_conditions_invalid_option_asset_class(self, get_api_key):
         """
-
+        This test case confirms that when we give an invalid option for asset class the returned response is empty
         :return:
         """
-        pass
+        logger.info("Now running Test case 13")
+        response = conditions.get_conditions(api_key=get_api_key, asset_class="Invalid_option")
+        logger.debug("Checking to see if returned response is none")
+        assert response.json()["results"] == [], \
+            f"Error! Unexpected value. Expected returned response: []. Actual: {response.json()['results']} "
+
+
 
 
