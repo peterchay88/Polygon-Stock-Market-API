d0ea8c77c0e0f58a45168e12b6388dded63906c7
Peter Chay <peterchay88@gmail.com>
2024-07-24 13:43:37 -0600

    Started on testcase 22-24. Still working out the bugs in how to check the conditions endpoint response

--- framework/src/helpers/conditions_helper.py
+++ framework/src/helpers/conditions_helper.py
@@ -1,4 +1,5 @@
 from framework.src.utils.logging_util import Logger
+from framework.src.helpers.regex_helper import regular_expression_checker
 
 logger = Logger()
 
@@ -11,10 +12,31 @@ def validate_conditions_response(conditions_response, **kwargs):
     """
     for key in kwargs:
         logger.debug(f"Checking if returned response only contain {key}: {kwargs[key]}")
-        for response_key in conditions_response:
-            if response_key[key] == kwargs[key]:
-                assert_value = True
-            else:
-                assert_value = False
-                break
+        for response in conditions_response: # Iterate through the list of returned dicts
+            # import pdb; pdb.set_trace()
+            for response_keys in response.keys(): # Iterate through the list of keys for the specific response
+                match = regular_expression_checker(pattern=key, string=response_keys)
+                if match:
+                    # pdb.set_trace()
+                    logger.debug(f"Checking to see if {response[response_keys]} matches {kwargs[key]}")
+                    if response[response_keys] == kwargs[key]:
+                        assert_value = True
+                    else:
+                        assert_value = False
+                        break
+
+            # if response[key] == kwargs[key]:
+            #     assert_value = True
+            # else:
+            #     assert_value = False
+            #     break
     return assert_value
+
+
+def validate_data_type(conditions_response, **kwargs):
+    """
+    This function is used to validate 'data_type' in the response
+    :param conditions_response:
+    :param kwargs:
+    :return:
+    """
--- framework/tests/conditions/test_get_conditions_positive.py
+++ framework/tests/conditions/test_get_conditions_positive.py
@@ -26,15 +26,36 @@ class TestConditionsPositive:
         pytest.param("options", "14", marks=pytest.mark.tcid14),
         pytest.param("crypto", "15", marks=pytest.mark.tcid15),
     ])
-    def test_get_conditions_asset_class_stocks(self, get_api_key, asset_class_param, test_id):
+    def test_get_conditions_asset_class(self, get_api_key, asset_class_param, test_id):
         """
-        This test case confirms if we specify only stocks for the asset class when calling the conditions endpoint
-        only data for stocks return
-
+        This test case confirms if we specify an argument for the asset class parameter when calling the conditions
+        endpoint only data for that argument return
         :return:
         """
         logger.info(f"Now running Test case {test_id}")
         params = {"asset_class": asset_class_param}
+        response = conditions.get_conditions(api_key=get_api_key, **params)
+        logger.debug(response.json())
+        assert_response = validate_conditions_response(response.json()["results"], **params)
+        assert assert_response is True, \
+            f"Error! Response returned an unexpected value"
+
+    @pytest.mark.parametrize("data_type_param,test_id", [
+        pytest.param("trade", "22", marks=pytest.mark.tcid22),
+        pytest.param("bbo", "23", marks=pytest.mark.tcid23),
+        pytest.param("nbbo", "24", marks=pytest.mark.tcid24)
+    ])
+    def test_get_conditions_data_type(self, get_api_key, data_type_param, test_id):
+        """
+        This test case confirms if we specify an argument for the data type parameter when calling the conditions
+        endpoint only data for that argument return
+        :param get_api_key:
+        :param data_type_param:
+        :param test_id:
+        :return:
+        """
+        logger.info(f"Now running Test case {test_id}")
+        params = {"data_type": data_type_param}
         response = conditions.get_conditions(api_key=get_api_key, **params)
         logger.debug(response.json())
         assert_response = validate_conditions_response(response.json()["results"], **params)
